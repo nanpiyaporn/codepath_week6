3e4a0afc61d1201f95878ddd93e1853bfe8ef55a
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-04-05 18:37:40 -0400

    Added LAyout from ./routes/Layout

--- crypto-hustle-pro/src/main.jsx
+++ crypto-hustle-pro/src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from './routes/Layout.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path ="/" element={<Layout />}>
+          <Route index={true} path="/" element={<App />} />
+          </Route>  
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
