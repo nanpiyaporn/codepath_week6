8f02c5bc2da57f43c078068b3c761998a11366ce
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-04-05 19:41:20 -0400

    Fix import path for DetailView component

--- crypto-hustle-pro/src/main.jsx
+++ crypto-hustle-pro/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from '/routes/Layout.jsx';
-import DetailView from './routes/DetailView';
+import DetailView from '/routes/DetailView.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
